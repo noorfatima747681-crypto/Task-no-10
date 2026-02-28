68fc727fba2f5118933f75d0febb8fcbad952594
noorfatima747681-crypto <noorfatima747681@gmail.com>
2026-02-28 01:33:54 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const courseName = "Modern Web Development";
+let currentCity = "Faisalabad";
+
+let studentName = "Noorfatima";
+let marks = 85;
+let ispassed = true;
+let futureplan;
+let scolarship = null;
+
+let userAge = prompt("Enter your age");
+let convertAge = Number(userAge);
+
+console.log("Marks type:" , typeof marks);
+console.log("Age type after conversation:" , typeof convertAge);
+console.log("Scolarship type:" , typeof scolarship);
+alert("Welcome " + "Noorfatima" + "! Your age is " + convertAge);
